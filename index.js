1d01ce4c48a3fe17fb23a0958a6ba2d22aff7577
khantseithu <khantseithuu@gmail.com>
2023-03-03 14:21:43 +0630

    finish up

--- index.js
+++ index.js
@@ -1,27 +1,8 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const slugify = require("slugify");
 const replaceTemplate = require("./modules/replaceTemplate");
-
-//synchronusly way
-// const textIn = fs.readFileSync('./txt/start.txt', 'utf-8');
-// console.log(textIn);
-// const textOut = `This is what we know about avocado: ${textIn}.\n Created on ${Date.now()}`
-// fs.writeFileSync('./txt/output.txt', textOut)
-// console.log("file written!")
-
-// // Callback Hell
-// fs.readFile('./txt/start.txt', 'utf-8',(err, data1) => {
-//     fs.readFile(`./txt/${data1}.txt`,'utf-8',(err,data2) => {
-//         console.log(data2);
-//         fs.readFile('./txt/append.txt','utf-8',(err,data3) => {
-//             console.log(data3);
-//             fs.writeFile('./txt/final.txt', `${data2}${data3}`, 'utf-8', err => {
-//                 console.log('File written!');
-//             })
-//         })
-//     })
-// })
 
 //Server
 
@@ -40,6 +21,11 @@ const tempProduct = fs.readFileSync(
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
+
+console.log(slugify("Fresh Avocados", { lower: true }));
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
